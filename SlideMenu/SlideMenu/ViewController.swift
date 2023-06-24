7694f3e0c262be326b443d853ec8a991f5345884
Thiago <Thiago Monteiro>
2023-06-24 01:20:23 -0300

    Created project Slide Out Menu App.

---
+++ SlideMenu/SlideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideMenu
+//
+//  Created by Thiago on 24/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
