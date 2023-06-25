56837b08667bc9616c9b06f7b91272b386a47a42
Thiago <Thiago Monteiro>
2023-06-25 20:40:01 -0300

    Created Home cell

---
+++ SlideMenu/SlideMenu/Features/Scene/Home/Cell/HomeCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeCell.swift
+//  SlideMenu
+//
+//  Created by Thiago on 25/06/23.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
