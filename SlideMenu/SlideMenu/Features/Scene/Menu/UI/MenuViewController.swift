5b9bc69c9c3e94de843e60cfe8af3e0423307f19
Thiago <Thiago Monteiro>
2023-06-25 17:37:42 -0300

    Updated number of rows.

--- SlideMenu/SlideMenu/Features/Scene/Menu/UI/MenuViewController.swift
+++ SlideMenu/SlideMenu/Features/Scene/Menu/UI/MenuViewController.swift
@@ -9,7 +9,7 @@ final class MenuViewController: UITableViewController {
 
 extension MenuViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 4
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
