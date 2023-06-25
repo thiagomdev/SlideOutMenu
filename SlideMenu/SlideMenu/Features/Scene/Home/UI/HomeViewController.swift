7e5f65d1b9bd1b632594df2e8a5e2fc286d0c755
Thiago <Thiago Monteiro>
2023-06-25 17:36:27 -0300

    Refactored initial menu view controller func and did perform animations func.

--- SlideMenu/SlideMenu/Features/Scene/Home/UI/HomeViewController.swift
+++ SlideMenu/SlideMenu/Features/Scene/Home/UI/HomeViewController.swift
@@ -1,21 +1,24 @@
 import UIKit
 
 final class HomeViewController: UITableViewController {
-    private lazy var mainWindow = UIApplication.shared.keyWindow
+    // MARK: - Properties
     private let menuWidth: CGFloat = 256
     
+    // MARK: - Components
     private lazy var menuViewController: MenuViewController = {
         let menu = MenuViewController()
-        menu.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: view.frame.height)
         return menu
     }()
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        setupMenuViewController()
     }
 }
 
+// MARK: - UITableViewController Protocols
 extension HomeViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
@@ -29,6 +32,7 @@ extension HomeViewController {
     }
 }
 
+// MARK: - HomeViewController private methods
 extension HomeViewController {
     private func setupNavigationItems(
         rightTitle: String,
@@ -51,25 +55,14 @@ extension HomeViewController {
         )
     }
     
-    @objc
-    private func handleRightButton() {
+    private func setupMenuViewController() {
+        menuViewController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: view.frame.height)
+        let mainWindow = UIApplication.shared.keyWindow
         mainWindow?.addSubview(menuViewController.view)
         addChild(menuViewController)
-        view.layoutIfNeeded()
-        UIView.animate(
-            withDuration: 0.9,
-            delay: .zero,
-            usingSpringWithDamping: 1,
-            initialSpringVelocity: 1,
-            options: .curveEaseOut
-        ) {
-            self.menuViewController.view.transform = CGAffineTransform(translationX: self.menuWidth, y: 0)
-            self.view.layoutIfNeeded()
-        }
     }
-    
-    @objc
-    private func handleLeftButton() {
+
+    private func perfomAnimations(by transform: CGAffineTransform) {
         UIView.animate(
             withDuration: 0.9,
             delay: .zero,
@@ -77,12 +70,28 @@ extension HomeViewController {
             initialSpringVelocity: 1,
             options: .curveEaseOut
         ) {
-            self.menuViewController.view.transform = .identity
+            self.menuViewController.view.transform = transform
+            self.view.transform = transform
             self.view.layoutIfNeeded()
         }
     }
 }
 
+// MARK: - HomeViewController @Selectors
+extension HomeViewController {
+    @objc
+    private func handleRightButton() {
+        setupMenuViewController()
+        perfomAnimations(by: .init(translationX: menuWidth, y: 0))
+    }
+    
+    @objc
+    private func handleLeftButton() {
+        perfomAnimations(by: .identity)
+    }
+}
+
+// MARK: - UI ViewConfiguration
 extension HomeViewController: ViewConfiguration {
     func buildViewHierarchy() {
         
