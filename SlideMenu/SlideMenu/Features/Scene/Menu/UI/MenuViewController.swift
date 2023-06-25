fd55299ebd7f57f304cbb9a5d66f77a2a6e66f05
Thiago <Thiago Monteiro>
2023-06-25 20:53:16 -0300

    Registed cell

--- SlideMenu/SlideMenu/Features/Scene/Menu/UI/MenuViewController.swift
+++ SlideMenu/SlideMenu/Features/Scene/Menu/UI/MenuViewController.swift
@@ -13,15 +13,23 @@ extension MenuViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "identifier")
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: MenuCell.identifier,
+            for: indexPath) as? MenuCell else { return UITableViewCell() }
         cell.selectionStyle = .none
-        cell.backgroundColor = .systemCyan
-        cell.textLabel?.text = "Row: \(indexPath.row)"
+//        cell.textLabel?.text = "Row: \(indexPath.row)"
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
+    }
+}
+
+extension MenuViewController {
+    private func registerCell() {
+//        tableView.separatorStyle = .none
+        tableView.register(MenuCell.self, forCellReuseIdentifier: MenuCell.identifier)
     }
 }
 
@@ -34,6 +42,7 @@ extension MenuViewController: ViewConfiguration {
     }
     
     func viewConfiguration() {
+        registerCell()
 //        tableView.separatorStyle = .none
         view.backgroundColor = .systemCyan
     }
